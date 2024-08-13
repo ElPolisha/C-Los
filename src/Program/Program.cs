a98c0de041d5db030556ca1db16936fe37201fb0
Cuete <adolfo.bravo@correo.ucu.edu.uy>
2024-08-13 19:12:12 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,7 +7,7 @@ public class Substraction
 {
     public static int Substract(int a, int b)
     {
-        return a - b;
+        return 0; //cambio incorrecto
     }
 }
 // Esta clase implementa la operación <division>
