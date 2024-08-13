5e70eb9c644c0bb2eac5041a0dec0f5771bd8be4
ElPolisha <lautaro.arrigoni@correo.ucu.edu.uy>
2024-08-13 18:48:37 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-
+// Esta clase implementa la operación <Adición>
 public class Addition
 {
     public static int Add(int a, int b)
@@ -11,7 +11,7 @@ public class Addition
         return a + b;
     }
 }
-
+// Esta clase implementa la operación <Multiplicación>
 public class Multiplication
 {
     public static int Multiply(int a, int b)
